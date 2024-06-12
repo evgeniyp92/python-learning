70e458cab5da4dc1e173989eb964bb22620581a0
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-06-12 15:01:14 +0200

    finish day 3

--- app1/main.py
+++ app1/main.py
@@ -7,9 +7,12 @@ while True:
     match user_action:
         case 'add':
             todo = str(input('Enter a todo: '))
-            todos.append(todo.capitalize())
-        case 'show':
+            todos.append(todo.capitalize().title())
+        case 'show' | 'display':
             for i in range(len(todos)):
                 print(f'{i+1}. {todos[i]}')
         case 'exit':
             break
+        # default case handling
+        case _:
+            print('You entered an invalid input')
