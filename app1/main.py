6e0a2e48aa1098002ec205d9483b3a69cbe5280e
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-06-12 14:10:32 +0200

    Refactor todo list input loop
    Refactored the main input loop in main.py to improve user interactions with the todo list. The new code allows users to switch between adding a new todo, showing all existing todos, or exiting the application, guided by more user-friendly prompts.

--- app1/main.py
+++ app1/main.py
@@ -1,11 +1,15 @@
+user_prompt = "Type add/show/exit: "
+
 todos = []
-awaiting_input = True
-while awaiting_input:
-    todo = str(input("Enter a todo or press q to quit: "))
-    if todo == 'q':
-        awaiting_input = False
-        continue
-    else:
-        todos.append(todo.capitalize())
-        continue
-print(todos)
+
+while True:
+    user_action = input(user_prompt).strip().lower()
+    match user_action:
+        case 'add':
+            todo = str(input('Enter a todo: '))
+            todos.append(todo.capitalize())
+        case 'show':
+            for i in range(len(todos)):
+                print(f'{i+1}. {todos[i]}')
+        case 'exit':
+            break
