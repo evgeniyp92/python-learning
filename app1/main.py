f123f1604e1a2c4244290d434d7a7448a23a6387
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-06-14 15:53:47 +0200

    add support for enumeration, and task completion

--- app1/main.py
+++ app1/main.py
@@ -1,18 +1,28 @@
-user_prompt = "Type add/show/exit: "
+user_prompt = "Type add/show/edit/finish/exit > "
 
 todos = []
 
 while True:
+    for index, item in enumerate(todos):
+        print(f"{index + 1}. {item}")
     user_action = input(user_prompt).strip().lower()
     match user_action:
         case 'add':
-            todo = str(input('Enter a todo: '))
+            todo = str(input('Enter a todo > '))
             todos.append(todo.capitalize().title())
         case 'show' | 'display':
             for i in range(len(todos)):
                 print(f'{i+1}. {todos[i]}')
+        case 'edit':
+            index = int(input('Enter the number of the todo to edit > '))
+            new_todo = str(input('Enter the edited todo > '))
+            todos[index - 1] = new_todo
+        case 'finish':
+            index = int(input('Enter the number of the todo to finish > ')) - 1
+            todos.pop(index)
         case 'exit':
             break
         # default case handling
         case _:
             print('You entered an invalid input')
+            continue
