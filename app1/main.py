a5b0d772290063acd80f9bcd119510c3fb2a5979
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-06-18 20:23:14 +0200

    Refactor user action handling from match-case to if-elif-else
    The code has been updated to replace the match-case statement with a series of if-elif-else conditions. In this update, the 'add', 'show', 'edit', 'finish', and, 'exit' actions are evaluated by checking whether their respective strings exist within the user input. Also, the 'add' and 'edit' actions now automatically extract the entered todo directly from the user input, and the offset for the 'finished' action has been corrected.

--- app1/main.py
+++ app1/main.py
@@ -4,71 +4,71 @@ while True:
     # for index, item in enumerate(todos):
     #     print(f"{index + 1}. {item}")
     user_action = input(user_prompt).strip().lower()
-    match user_action:
-        case 'add':
-            # Get input from user
-            todo = input('Enter a todo > ') + "\n"
+    if 'add' in user_action:
+        # Get input from user
+        # todo = input('Enter a todo > ') + "\n"
+        todo = user_action[4:] + "\n"
 
-            # Using context manager, no need to close files
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        # Using context manager, no need to close files
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Append the new todo
-            todos.append(todo.capitalize().title())
+        # Append the new todo
+        todos.append(todo.capitalize().title())
 
-            # Open the file again in write mode and write lines again
-            # basic modes: w-write (blowaway), r-read
-            # refactored with context manager
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Open the file again in write mode and write lines again
+        # basic modes: w-write (blowaway), r-read
+        # refactored with context manager
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'show' | 'display':
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif 'show' in user_action:
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Cleaning up a list via a for loop
-            new_todos = []
-            for item in todos:
-                new_todos.append(item.strip('\n'))
+        # Cleaning up a list via a for loop
+        new_todos = []
+        for item in todos:
+            new_todos.append(item.strip('\n'))
 
-            # Cleaning up using list comprehension
-            new_todos2 = [item.strip('\n') for item in todos]
+        # Cleaning up using list comprehension
+        new_todos2 = [item.strip('\n') for item in todos]
 
-            # for i in range(len(new_todos)):
-            #     print(f'{i+1}. {new_todos[i]}')
+        # for i in range(len(new_todos)):
+        #     print(f'{i+1}. {new_todos[i]}')
 
-            for index, item in enumerate(new_todos):
-                print(f'{index+1}. {item}')
+        for index, item in enumerate(new_todos):
+            print(f'{index + 1}. {item}')
 
-        case 'edit':
-            index = int(input('Enter the number of the todo to edit > '))
+    elif 'edit' in user_action:
+        index = int(user_action[5:])
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            new_todo = str(input('Enter the edited todo > '))
-            todos[index - 1] = new_todo.capitalize().title()
+        new_todo = str(input('Enter the edited todo > '))
+        todos[index - 1] = new_todo.capitalize().title() + '\n'
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'finish':
-            index = int(input('Enter the number of the todo to finish > ')) - 1
+    elif 'finish' in user_action:
+        index = int(user_action[7:]) - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.pop(index - 1)
+        todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            print('Todo finished successfully')
+        print('Todo finished successfully')
 
-        case 'exit':
-            break
+    elif 'exit' in user_action:
+        break
 
-        # default case handling
-        case _:
-            print('You entered an invalid input')
-            continue
+    # default case handling
+    else:
+        print('You entered an invalid input')
+        continue
