39e9fa5fe205798e748d66fda1dccb6af48c5165
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-07-03 09:41:04 +0200

    add match case statement to handle different events and refactor code visually for better appearance

--- app1/gui.py
+++ app1/gui.py
@@ -2,9 +2,31 @@ import functions
 import FreeSimpleGUI as gui
 
 label = gui.Text('Type in a todo')
-input_box = gui.InputText(tooltip='Enter a todo')
+input_box = gui.InputText(tooltip='Enter a todo', key='todo')
 add_button = gui.Button('Add')
 
-window = gui.Window('My Todo App', layout=[[label], [input_box], [add_button]])
-window.read()
+window = gui.Window('My Todo App',
+                    layout=[
+                        [label], [input_box], [add_button]
+                        ],
+                    font=('Helvetica', 20)
+                    )
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case 'Edit':
+            pass
+        case gui.WIN_CLOSED:
+            break
+        case _:
+            pass
+
 window.close()
