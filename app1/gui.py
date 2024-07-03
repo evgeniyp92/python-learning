e945cabc3790af61fad7d5e0cae15852d5076cfb
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-07-03 10:05:38 +0200

    Add listbox and edit functionality to GUI
    The GUI was updated to include a listbox which displays current todos. The script now fetches the existing todos at startup and lists them in the new listbox. Also, an "Edit" button was added to allow the user to select a todo from the list and edit its content. The edited todo is then updated in the list and saved to the disk.

--- app1/gui.py
+++ app1/gui.py
@@ -4,12 +4,16 @@ import FreeSimpleGUI as gui
 label = gui.Text('Type in a todo')
 input_box = gui.InputText(tooltip='Enter a todo', key='todo')
 add_button = gui.Button('Add')
+list_box = gui.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
+edit_button = gui.Button('Edit')
 
 window = gui.Window('My Todo App',
                     layout=[
-                        [label], [input_box], [add_button]
+                        [label],
+                        [input_box, add_button],
+                        [list_box, edit_button]
                         ],
-                    font=('Helvetica', 20)
+                    font=('Helvetica', 16)
                     )
 
 while True:
@@ -23,7 +27,15 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
         case 'Edit':
-            pass
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case gui.WIN_CLOSED:
             break
         case _:
