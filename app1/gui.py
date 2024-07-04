e8e749895987974ecf1623703acab11f2f59777e
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-07-04 10:10:43 +0200

    Add real-time clock and improve error handling in GUI
    A real-time clock has been incorporated into the GUI and it updates every 500 milliseconds. Error handling has also been improved, specifically for the 'Edit' button where an IndexError was caught and handled with a descriptive message to the user to select an item first before editing. The 'Exit' button's size has been increased for better visibility.

--- app1/gui.py
+++ app1/gui.py
@@ -1,16 +1,23 @@
+import time
+
 import functions
 import FreeSimpleGUI as gui
+import os
 
+
+
+clock = gui.Text('', key='clock')
 label = gui.Text('Type in a todo')
 input_box = gui.InputText(tooltip='Enter a todo', key='todo')
 add_button = gui.Button('Add')
 list_box = gui.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
 edit_button = gui.Button('Edit')
 complete_button = gui.Button('Complete')
-exit_button = gui.Button('Exit')
+exit_button = gui.Button('Exit', size=60)
 
 window = gui.Window('My Todo App',
                     layout=[
+                        [clock],
                         [label],
                         [input_box, add_button],
                         [list_box, edit_button, complete_button],
@@ -20,7 +27,8 @@ window = gui.Window('My Todo App',
                     )
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=500)
+    window['clock'].update(value=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     print(event)
     print(values)
     match event:
@@ -32,13 +40,16 @@ while True:
             window['todos'].update(values=todos)
             window['todo'].update(value='')
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n"
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                gui.popup('Please select an item first', font=("Helvetica", 16))
         case 'Complete':
             todo_to_complete = values['todos'][0]
             todos = functions.get_todos()
