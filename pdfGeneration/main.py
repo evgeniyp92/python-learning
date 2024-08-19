6f86e0798930139f93350e87737c2f3acf68a447
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-08-19 11:06:45 +0200

    Add initial PDF generation script and configuration files
    This commit includes a script using pandas and fpdf libraries to generate a PDF from `topics.csv`. Additionally, various IDE configuration files are added, setting up project properties, version control, and file attributes.

---
+++ pdfGeneration/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from fpdf import FPDF
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv('topics.csv')
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font("Helvetica", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="l", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+pdf.output("output.pdf")
