f0f262f9a638948d22698dac7e2845b1349bc562
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-08-19 15:03:01 +0200

    Generate PDF content dynamically
    Moved Excel data reading and processing logic to after initiating the PDF object. Added dynamic headers and row data processing from the Excel file to improve invoice generation.

--- excelToPDFInvoice/main.py
+++ excelToPDFInvoice/main.py
@@ -5,9 +5,6 @@ from fpdf import FPDF
 filepaths = glob.glob("invoices/*.xlsx")
 
 for file in filepaths:
-    # Read data from excel
-    df = pd.read_excel(file, sheet_name="Sheet 1")
-
     # Init pdf object
     pdf = FPDF(orientation="P", unit="mm", format="A4")
 
@@ -26,6 +23,39 @@ for file in filepaths:
     date = '.'.join(date)
     pdf.set_font("Helvetica", size=12)
     pdf.cell(w=50, h=8, txt=f'Date: {date}', ln=1)
+
+    # Read data from excel
+    df = pd.read_excel(file, sheet_name="Sheet 1")
+
+    headers = df.columns.tolist()
+
+    headers = [item.replace('_', ' ').title() for item in headers]
+
+    pdf.set_font('Helvetica', size=10, style='B')
+    pdf.cell(w=30, h=8, txt=f'{headers[0]}', border=1)
+    pdf.cell(w=70, h=8, txt=f'{headers[1]}', border=1)
+    pdf.cell(w=30, h=8, txt=f'{headers[2]}', border=1)
+    pdf.cell(w=30, h=8, txt=f'{headers[3]}', border=1)
+    pdf.cell(w=30, h=8, txt=f'{headers[4]}', border=1, ln=1)
+    pdf.set_font(family='Helvetica', size=12, style='')
+
+    # pdf.set_font('Helvetica', size=12, style='B')
+    # pdf.cell(w=30, h=8, txt=f'Product ID', border=1)
+    # pdf.cell(w=70, h=8, txt=f'Product Name', border=1)
+    # pdf.cell(w=30, h=8, txt=f'Amount', border=1)
+    # pdf.cell(w=30, h=8, txt=f'Price/Unit', border=1)
+    # pdf.cell(w=30, h=8, txt=f'Total', border=1, ln=1)
+    # pdf.set_font(family='Helvetica', size=12, style='')
+
+    for index, row in df.iterrows():
+        pdf.cell(w=30, h=8, txt=f'{row["product_id"]}', border=1)
+        pdf.cell(w=70, h=8, txt=f'{row["product_name"]}', border=1)
+        pdf.cell(w=30, h=8, txt=f'{row["amount_purchased"]}', border=1)
+        pdf.cell(w=30, h=8, txt=f'{row["price_per_unit"]}', border=1)
+        pdf.cell(w=30, h=8, txt=f'{row["total_price"]}', border=1, ln=1)
+
+    pdf.set_font(family='Helvetica', style='B')
+    pdf.cell(w=0, h=8, txt="Prolaps Motorsports", ln=1)
 
     # Write to file
     pdf.output(f'PDFs/{invoice_number}.pdf')
