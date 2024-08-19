1dc8ef089e3e7b1c517ff6fd457cab9164adb4bf
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-08-19 14:44:19 +0200

    Refactor invoice number extraction and add date field
    Improved the extraction logic for invoice numbers and incorporated the invoice date into the PDF output. This change corrects file parsing and enriches the generated PDFs with additional details.

--- excelToPDFInvoice/main.py
+++ excelToPDFInvoice/main.py
@@ -3,19 +3,29 @@ import glob
 from fpdf import FPDF
 
 filepaths = glob.glob("invoices/*.xlsx")
-print(filepaths)
 
 for file in filepaths:
     # Read data from excel
     df = pd.read_excel(file, sheet_name="Sheet 1")
+
     # Init pdf object
     pdf = FPDF(orientation="P", unit="mm", format="A4")
+
     # Add a page and style the font
     pdf.add_page()
     pdf.set_font("Helvetica", size=16)
+
     # Get the invoice number from the filepath
-    invoice_number = file.split('.')[0].split('/')[1]
+    invoice_number = file.split('/')[-1].split('.')[:-1]
+    invoice_number = '.'.join(invoice_number)
+
     # Write the header
-    pdf.cell(w=50, h=8, txt=f'Invoice #{invoice_number}', ln=1)
+    pdf.cell(w=50, h=8, txt=f'Invoice #{invoice_number.split('-')[0]}', ln=1)
+
+    date = file.split('-')[1].split('.')[:-1]
+    date = '.'.join(date)
+    pdf.set_font("Helvetica", size=12)
+    pdf.cell(w=50, h=8, txt=f'Date: {date}', ln=1)
+
     # Write to file
     pdf.output(f'PDFs/{invoice_number}.pdf')
